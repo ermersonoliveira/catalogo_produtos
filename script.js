1c18d0f59c51ce646ba0a7bd4083664bfa3c4021
ermersonoliveira <ermerson.dev@gmail.com>
2025-11-20 15:42:42 -0300

    before gemini integrations

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+let cardContainer = document.querySelector(".card-container");
+
 let dados = [];
 
-async function buscarProdutos(){
+async function buscarProdutos() {
     let resposta = await fetch("data.json");
     dados = await resposta.json();
-    console.log(dados);
+    renderizarCards(dados);
+}
+
+function renderizarCards(dados) {
+    for (let dado of dados) {
+        let article = document.createElement("article");
+        article.classList.add("card");
+        article.innerHTML = `
+        <h2>${dado.nome}</h2>
+        <p>${dado.descricao}</p>
+        <a href="${dado.link}">Saiba mais</a>
+        `
+        cardContainer.appendChild(article);
+
+    }
 }
