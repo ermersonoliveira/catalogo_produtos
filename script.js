21a4786f8e6a07de6c4ef7a720e6d174668edeb2
ermersonoliveira <ermerson.dev@gmail.com>
2025-11-21 00:35:39 -0300

    add update article

--- script.js
+++ script.js
@@ -57,6 +57,14 @@ function renderizarCards(dados) {
         <p>${dado.descricao}</p>
         <a href="#" class="product-category" data-category="${dado.categoria}">${dado.categoria}</a>
         <p class="preco">${dado.preco}</p>
+        <div class="availability-warning">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="12" cy="12" r="10"></circle>
+                <line x1="12" y1="8" x2="12" y2="12"></line>
+                <line x1="12" y1="16" x2="12.01" y2="16"></line>
+            </svg>
+            <span>Por favor, consultar a disponibilidade do produto.</span>
+        </div>
         <a href="${dado.link}" target="_blank">Saiba mais</a>
         `
         cardContainer.appendChild(article);
